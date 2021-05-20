85f510c34cc7ded3f3ea3d6f99d2b6148515ca49
Gustavo Franco <gu.franco13@gmail.com>
2021-05-20 14:23:46 -0400

    aula1

---
+++ Contents.swift
@@ -0,0 +1,134 @@
+import UIKit
+
+
+//isso eh uma string
+let issoEhUmaString : String = "String"
+
+//isso eh uma caracter
+let Character: Character = "A"
+
+//isso eh um intinger
+let numeroUm: Int = 1
+
+//isso eh um float
+let numeroFloat: Float = 25.255
+
+//isso eh um double
+let numeroDouble: Double = 25.25252525
+
+//isso eh uma array de strings in swift
+let streets: [String] = ["Ruas das Acacias", "rua dos flamboyans", "rua das rosas"]
+
+//isso eh uma Array de Int in swift
+let numeroImpares: [Int] = [1, 3, 5, 7, 9, 11, 13, 15]
+
+//isso eh um dicionario
+let dic	= ["nome": "Zeus", "idade": "79"]
+
+let numeros	= [1: "Um", 2: "Dois", 3: "Tres"]
+
+// isso eh um boolian
+let euSouLegal = false
+print(euSouLegal)
+
+
+//Inicializacao de variaveis
+var nome = String ()
+print(nome)
+nome = "Gustavo Franco"
+
+var idade = Int()
+
+idade = 20
+
+var altura = Double()
+
+altura = 1.70
+print(altura)
+
+
+//Isso eh interpolacao
+
+print("Ola, meu nome eh \(nome), eu tenho \(idade) anos de idade")
+
+
+//isso eh uma concatenacao
+
+var animal = "Cachorro"
+var tipo  = " Spitz"
+
+
+print(animal + tipo)
+
+
+
+
+// Operador de atribuicao
+
+var numero = 100
+
+numero = 200
+
+print(numero)
+
+
+var (numero1, numero2, numero3) = (100, 200, 300)
+
+//operadores aritimaticos
+
+//some
+let soma = 2 + 2
+print(soma)
+
+//subtracao
+let subtracao = 10 - 7
+print(subtracao)
+
+//multiplicacao
+let multiplicacao = 100 * 5
+print(multiplicacao)
+
+
+//divisao
+let divisao = 200 / 4
+print(divisao)
+
+//resto, porcentagem
+
+let resto = 33 % 5
+print(resto)
+
+// inversao de valores de positivo para negativo
+
+var numeroCem = 100
+
+print(-numeroCem)
+
+
+
+
+var valor = 250
+// valor mais 10
+print(valor+10)
+
+
+//O preco eh igual a preco menos 10.10
+
+var preco = 300
+preco -= Double(10.10)
+
+
+//o preco eh igual a preco vezes 1.78
+
+
+
+
+//o preco eh igual a preco dividido por 3
+
+
+
+
+
+	
+
+
